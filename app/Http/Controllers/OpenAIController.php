1bcb2ba6c04995dd3a53497a64feb0af3d0b06a4
Timothy Mostert <resident@hotmail.co.za>
2023-04-11 16:55:22 +0800

    init commit

---
+++ app/Http/Controllers/OpenAIController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use OpenAI\Laravel\Facades\OpenAI as OpenAI;
+
+class OpenAIController extends Controller
+{
+    public function prompt(Request $request)
+    {
+        $request->validate([
+            'prompt' => 'required',
+        ]);
+
+        $result = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $request->prompt,
+        ]);
+        
+        return response()->json($result);
+    }
+}
